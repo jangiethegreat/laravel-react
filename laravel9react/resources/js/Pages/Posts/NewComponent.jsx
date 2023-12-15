bfe4563f84183e6926e0d117f4c387a78aec6d23
jangiethegreat <fernandojohncris@gmail.com>
2023-12-15 11:40:13 +0800

    using component state

--- laravel9react/resources/js/Pages/Posts/NewComponent.jsx
+++ laravel9react/resources/js/Pages/Posts/NewComponent.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 
 export default class NewComponent extends Component{
+    state = {
+        name:'Jang',
+        age:22,
+        mobile:1234567890,
+        skills:[
+            'react',
+            'javascript',
+            'laravel',
+
+        ]
+    }
     render(){
         return(
             <div>
                 <h1>This is a Class Component</h1>
+
+                <p> Name: {this.state.name}</p>
+                <p> Age: {this.state.age}</p>
+                <p> Mobile: {this.state.mobile}</p>
+                <p> Skills: {this.state.skills.join(', ')}</p>
             </div>
         )
     }
