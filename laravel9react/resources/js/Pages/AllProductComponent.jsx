c5aa9ebd82280463c4c8dd8cffda7c9b653f6fdf
jangiethegreat <fernandojohncris@gmail.com>
2023-12-20 11:24:39 +0800

    props product ffunction

---
+++ laravel9react/resources/js/Pages/AllProductComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";  
+
+import ProductComponent from "./ProductComponent";
+
+export default function AllProductComponent(){
+    const product = {
+        Name: 'First Product',
+        Price: 200,
+        Stock: 'InStock'
+    }
+
+    return(
+        <>
+        <ProductComponent product ={product} />
+        </>
+    )
+}
